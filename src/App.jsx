c91651fe796ecccb98f67eaea72d9f9f111d73e8
vani <vanibodasingu.com>
2025-07-03 09:15:56 +0530

    p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 
 // Remove these imports if the files do not exist or are not needed
